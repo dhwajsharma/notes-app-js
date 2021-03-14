594f172bdc8375544cad67e8855a74d194486d38
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-03-14 22:25:09 +0530

    added localStorage

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const addBtn = document.getElementById("add");
 
+const notes = JSON.parse(localStorage.getItem("notes"));
+
+if (notes) {
+    notes.forEach(note => addNewNote(note));
+}
+
 addBtn.addEventListener("click", () => addNewNote())
 
-const addNewNote = (text = "") => {
+function addNewNote(text = "") {
     const note = document.createElement("div");
     note.classList.add("note")
 
@@ -22,9 +28,16 @@ const addNewNote = (text = "") => {
     const textArea = note.querySelector("textarea");
 
 
+    textArea.value = text;
+    main.innerHTML = marked(text);
+
     deleteBtn.addEventListener("click", () => {
         note.remove();
+
+        updateLS();
     })
+
+
 
     editBtn.addEventListener("click", () => {
         main.classList.toggle("hidden");
@@ -35,8 +48,20 @@ const addNewNote = (text = "") => {
         const { value } = e.target
 
         main.innerHTML = marked(value);
+
+        updateLS();
     })
 
 
     document.body.appendChild(note)
+}
+
+const updateLS = () => {
+    const notesText = document.querySelectorAll("textarea");
+
+    const notes = [];
+
+    notesText.forEach(note => notes.push(note.value));
+
+    localStorage.setItem("notes", JSON.stringify(notes));
 }
